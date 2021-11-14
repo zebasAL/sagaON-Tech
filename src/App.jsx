feadca92d6d2c06649eee7f9076b8da500b4c746
zebasAL <zebastian_al@hotmail.com>
2021-11-14 17:27:03 -0600

    challenge 1

---
+++ src/App.jsx
@@ -0,0 +1,83 @@
+import React, {useState} from 'react';
+import CSVReader from 'react-csv-reader';
+import CsvDownloader from 'react-csv-downloader';
+import './App.css';
+import {handlesMatchedValues} from './utilities';
+
+const App = () => {
+const [firstInstructionResult, setFirstInstructionResult] = useState(null);
+const [secondInstructionResult, setSecondInstructionResult] = useState(null);
+
+const dataResults = [{
+  firstInstruction: firstInstructionResult,
+  secondInstruction: secondInstructionResult
+}];
+
+/**
+ * Handle uploadedFile data
+ * @param {Array} data 
+ * @returns {void} 
+ */
+const handleUploadedFile = (data) => {
+  //Set Entry values
+  const [charactersLength, firstInstruction, secondInstruction, message] = data;
+
+  //Gets the separated values ​​by whitespace
+  const [firstInstructionLength, secondInstructionLength, messageLength] = charactersLength.toString().split(/\s+/);
+
+  //Sets validation requirements
+  if (charactersLength && firstInstruction && secondInstruction && message
+    && (messageLength >= 3 && messageLength <= 5000)
+    && (firstInstructionLength >= 2 && firstInstructionLength <= 50)
+    && (secondInstructionLength >= 2 && secondInstructionLength <= 50)) {
+      setFirstInstructionResult(handlesMatchedValues(firstInstruction.toString(), message.toString()));
+      setSecondInstructionResult(handlesMatchedValues(secondInstruction.toString(), message.toString()));
+  } else {
+
+    // reset input field when file doesn't match requeriments
+    alert('file data  does not match requeriments');
+    document.querySelector('#react-csv-reader-input').value = '';
+  }
+}
+
+  return (
+    <div className="App">
+      <body className="App-header">
+
+        <CSVReader 
+          id="csv-reader"
+          onFileLoaded={(data) => handleUploadedFile(data)}
+        />
+
+        <input
+          class="csv-input"
+          type="text"
+          value={(document.querySelector('#react-csv-reader-input'))
+            ? document.querySelector('#react-csv-reader-input').value
+            : 'Drag your CSV file with requested format'
+          }
+        />
+       
+        <CsvDownloader
+          className="csv-downloader"
+          disabled={firstInstructionResult === null || secondInstructionResult === null}
+          filename="results"
+          extension=".csv"
+          datas={dataResults}
+          text="Download results"
+        />
+
+        <a
+          className="sample-downloader"
+          download
+          href="/test-challenge.csv"
+        >
+          Download sample CSV file
+        </a>
+
+      </body>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/utilities.js
@@ -0,0 +1,29 @@
+/**
+ * validates characters and replace duplicated characters
+ * @param {string} message 
+ * @returns {string}
+ */
+export const handlesRepeatedValues = (message) => {
+  return message.toLowerCase().replace(/[^\w\s]|(.)\1{1,}/g, (str, match) => {
+    return match[0];
+  })
+};
+
+/**
+ * handlesMatchedValues
+ * @param {string} instruction 
+ * @param {string} message 
+ * @returns {boolean}
+ */
+export const handlesMatchedValues = (instruction, message) =>{
+  const fixedMessage = handlesRepeatedValues(message);
+  if (fixedMessage.includes(instruction.toLowerCase())) {
+    return true
+  } else {
+    return false
+  }
+}
+
+
+
+
